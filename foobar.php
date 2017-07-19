43116cfde582c75bd6f90225c732b81dd22174a5
tifenn <tifenn@Debian.lan>
2017-07-19 10:40:17 +1000

    First commit + foobar.php => DONE

---
+++ foobar.php
@@ -0,0 +1,16 @@
+<?php
+
+for ($i = 1; $i <= 100; $i++) {
+    if (($i % 3 == 0) && ($i % 5 == 0)) {
+        echo 'foobar, ';
+    }
+    elseif ($i % 3 == 0) {
+        echo 'foo, ';
+    }
+    elseif ($i % 5 == 0) {
+        echo 'bar, ';
+    }
+    else {
+        echo $i . ', ';
+    }
+}
