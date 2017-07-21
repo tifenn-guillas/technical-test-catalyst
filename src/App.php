33d48aa7a04ceca957da1969ab76a7c9ee471598
tifenn <tifenn@Debian.lan>
2017-07-21 12:34:31 +1000

    Parse file function => DONE

--- src/App.php
+++ src/App.php
@@ -2,6 +2,7 @@
 
 namespace Catalyst;
 
+use Catalyst\Entity\User;
 use Doctrine\ORM\EntityManager;
 
 class App
@@ -24,12 +25,18 @@ class App
         $this->em = $em;
         $this->argsManager = $argsManager;
         $this->checkDatabaseConnection();
-        $this->checkExistingFile();
+        if (!$this->argsManager->getCreateTable()) {
+            $this->checkExistingFile();
+        }
     }
 
     public function run() {
-        // TODO: parse file
-        // TODO: create table
+        if ($this->argsManager->getCreateTable()) {
+            // TODO: create table
+            echo 'CREATE TABLE' . PHP_EOL;
+        } else {
+            $this->parseFile();
+        }
     }
 
     private function checkDatabaseConnection() {
@@ -49,5 +56,52 @@ class App
             echo 'No such file or directory in ' . $file . PHP_EOL;
             exit;
         }
+    }
+
+    private function createTable() {}
+
+    private function parseFile() {
+        $file = fopen($this->argsManager->getFile(), 'r');
+        while (($line = fgets($file)) !== false) {
+            $tabLine = explode(',', $line);
+            if ($this->checkNbCol($tabLine) and $this->checkName($tabLine) and $this->checkEmail($tabLine)) {
+                $user = new User();
+                $user->setName(ucwords(strtolower(trim($tabLine[0]))));
+                $user->setSurname(ucwords(strtolower(trim($tabLine[1]))));
+                $user->setEmail(strtolower(trim($tabLine[2])));
+                $this->em->persist($user);
+            }
+        }
+        fclose($file);
+        if (!$this->argsManager->getDryRun()) {
+//            $this->em->flush(); // TODO: test ORM
+            echo PHP_EOL . 'All correct records are stored.' . PHP_EOL;
+        }
+    }
+
+    private function checkNbCol($array) {
+        if (count($array) != 3) {
+            echo 'Wrong format: ' . implode(',', $array); // TODO: Throwing an Exception
+            return false;
+        }
+        return true;
+    }
+
+    private function checkName($array) {
+        $firstname = trim($array[0]);
+        $lastname = trim($array[1]);
+        if (!preg_match("/^[a-zA-Z\s-']*$/", $firstname) or !preg_match("/^[a-zA-Z\s-']*$/", $lastname)) {
+            echo 'Invalid name: ' . implode(',', $array); // TODO: Throwing an Exception
+            return false;
+        }
+        return true;
+    }
+
+    private function checkEmail($array) {
+        if (!filter_var(trim($array[2]), FILTER_VALIDATE_EMAIL)) {  // TODO: documentation filter email
+            echo 'Invalid email: ' . implode(',', $array); // TODO: Throwing an Exception
+            return false;
+        }
+        return true;
     }
 }
